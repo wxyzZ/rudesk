2cf1e0d69befbf4d8b4762af08063d9635e29564
test <test@abc.com>
2024-12-24 10:26:57 +0800

    1

--- res/msi/preprocess.py
+++ res/msi/preprocess.py
@@ -183,7 +183,7 @@ def replace_app_name_in_langs(app_name):
         with open(file_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
         for i, line in enumerate(lines):
-            lines[i] = line.replace("homedesk", app_name)
+            lines[i] = line.replace("RustDesk", app_name)
         with open(file_path, "w", encoding="utf-8") as f:
             f.writelines(lines)
 
@@ -482,8 +482,8 @@ def update_license_file(app_name):
     license_file = Path(sys.argv[0]).parent.joinpath("Package/License.rtf")
     with open(license_file, "r", encoding="utf-8") as f:
         license_content = f.read()
-    license_content = license_content.replace("website homedesk.com and other ", "")
-    license_content = license_content.replace("homedesk", app_name)
+    license_content = license_content.replace("website rustdesk.com and other ", "")
+    license_content = license_content.replace("RustDesk", app_name)
     license_content = re.sub("Purslane Ltd", app_name, license_content, flags=re.IGNORECASE)
     with open(license_file, "w", encoding="utf-8") as f:
         f.write(license_content)
