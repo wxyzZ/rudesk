98f5ca7c22c63246fb1ac6fb029157a03c173207
test <test@abc.com>
2024-12-24 17:56:03 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -48,10 +48,10 @@ fn main() {
         -k, --key=[KEY] ''
        -s, --server=[] 'Start server'",
     );
-    let matches = App::new("rustdesk")
+    let matches = App::new("homedesk")
         .version(crate::VERSION)
-        .author("Purslane Ltd<info@rustdesk.com>")
-        .about("RustDesk command line tool")
+        .author("Purslane Ltd<info@homedesk.com>")
+        .about("homedesk command line tool")
         .args_from_usage(&args)
         .get_matches();
     use hbb_common::{config::LocalConfig, env_logger::*};
